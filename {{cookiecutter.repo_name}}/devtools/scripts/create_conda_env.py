76cb02edd13152efcd2162f296dd8d942cfca043
Levi Naden <lnaden@vt.edu>
2019-01-04 15:35:29 -0500

    Small update to env script while we work though details
    Enforces Pyyaml be installed for the env creation script
    We thought about using ruamel_yaml like Conda does, but thats only in
    the base env, so we might as well enforce a more common YAML parser
    if we are going to need one

--- {{cookiecutter.repo_name}}/devtools/scripts/create_conda_env.py
+++ {{cookiecutter.repo_name}}/devtools/scripts/create_conda_env.py
@@ -1,52 +1,56 @@
 import argparse
-import yaml
+import yaml  # Requires PyYAML
 import os
 import re
 import shutil
 import subprocess as sp
+from tempfile import NamedTemporaryFile
 
 # Args
 parser = argparse.ArgumentParser(description='Creates a conda environment from file for a given Python version.')
-parser.add_argument('-n', '--name', type=str, nargs=1,
+parser.add_argument('-n', '--name', type=str,
                     help='The name of the created Python environment')
-parser.add_argument('-p', '--python', type=str, nargs=1,
+parser.add_argument('-p', '--python', type=str,
                     help='The version of the created Python environment')
-parser.add_argument('conda_file', nargs='*',
+parser.add_argument('conda_file',
                     help='The file for the created Python environment')
 
 args = parser.parse_args()
 
 # Open the base file
-with open(args.conda_file[0], "r") as handle:
+with open(args.conda_file, "r") as handle:
     yaml_script = yaml.load(handle.read())
-
-tmp_file = "tmp_env.yaml"
 
 # Make changes based on the options
 # Python
 python_replacement_string = "python {}*".format(args.python[0])
-try:  # Python
+try:
     for dep_index, dep_value in enumerate(yaml_script['dependencies']):
         if re.match('python([ ><=*]+[0-9.*]*)?$', dep_value):  # Match explicitly 'python' and its formats
-            yaml_script['dependencies'][dep_index] = python_replacement_string
-            break  # Making the assumption there is only one Python entry
+            yaml_script['dependencies'].pop(dep_index)
+            break  # Making the assumption there is only one Python entry, also avoids need to enumerate in reverse
 except KeyError:
     # Case of no dependencies key
-    yaml_script['dependencies'] = [python_replacement_string]
-
-with open(tmp_file, "w") as handle:
-    handle.write(yaml.dump(yaml_script))
+    yaml_script['dependencies'] = []
+finally:
+    # Ensure the python version is added in. Even if the code does not need it, we assume the env does
+    yaml_script['dependencies'].insert(0, python_replacement_string)
 
 # Figure out conda path
 if "CONDA_EXE" in os.environ:
     conda_path = os.environ["CONDA_EXE"]
 else:
     conda_path = shutil.which("conda")
+if conda_path is None:
+    raise RuntimeError("Could not find a conda binary in CONDA_EXE variable or in executable search path")
 
-print("CONDA ENV NAME  {}".format(args.name[0]))
-print("PYTHON VERSION  {}".format(args.python[0]))
-print("CONDA FILE NAME {}".format(args.conda_file[0]))
+print("CONDA ENV NAME  {}".format(args.name))
+print("PYTHON VERSION  {}".format(args.python))
+print("CONDA FILE NAME {}".format(args.conda_file))
 print("CONDA PATH      {}".format(conda_path))
 
-sp.call("{} env create -n {} -f {}".format(conda_path, args.name[0], tmp_file), shell=True)
-os.remove(tmp_file)
+# Write to a temp file which will always be cleaned up
+with NamedTemporaryFile() as temp_file:
+    temp_file.write(yaml.dump(yaml_script).encode())  # Temp files opened as binaries, must encode
+    temp_file.seek(0)  # Ensure head is moved to start since file never closes (it will delete if closed)
+    sp.call("{} env create -n {} -f {}".format(conda_path, args.name, temp_file.name), shell=True)
--- {{cookiecutter.repo_name}}/devtools/travis-ci/before_install.sh
+++ {{cookiecutter.repo_name}}/devtools/travis-ci/before_install.sh
@@ -35,7 +35,7 @@ source ~/.bashrc  # source file to get new commands
 conda config --add channels conda-forge
     {% endif %}
 conda config --set always_yes yes
-conda install conda conda-build jinja2 anaconda-client
+conda install conda conda-build jinja2 anaconda-client pyyaml
 conda update --quiet --all
 {% elif cookiecutter.dependency_source == 'Dependencies from pip only (no conda)' %}
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
